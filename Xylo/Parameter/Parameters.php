c57a308eb4a29dbe5c41581020f84046b60f274f
whirlwind <whirlwind@debian-fixe>
2015-11-05 08:41:43 +0100

    Add skeleton of Parameters management

---
+++ Xylo/Parameter/Parameters.php
@@ -0,0 +1,12 @@
+<?php
+namespace Xylo\Parameter;
+
+class Parameters
+{
+    public function __construct(array $paramsRoute)
+    {
+        switch ($paramsRoute['method']) {
+            // Todo
+        }
+    }
+}
