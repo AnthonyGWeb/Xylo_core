052f4d504a53878c9eb6731c435733a6ae5ad44c
AnthonyGWeb <grindatto.a@gmail.com>
2015-11-28 23:34:54 +0100

    Delete deprecated folder and update .gitignore

--- Portfolio/www/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("ROOT_PATH", __DIR__ . '/../../');
-
-require_once ROOT_PATH . 'vendor/autoload.php';
-
-$application = new \Xylo\Application\Application();
-$application->init();
-$application->run();
