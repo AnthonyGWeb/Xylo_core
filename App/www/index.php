7b6d98c051a8dff517f79c3f8e70d41f001b02df
whirlwind <whirlwind@debian-fixe>
2015-11-28 23:07:13 +0100

    Add new config exemple and Folders App

---
+++ App/www/index.php
@@ -0,0 +1,12 @@
+<?php
+define("ROOT_PATH", __DIR__ . '/../../');
+define("APP_PATH", __DIR__ . '/../../App/');
+
+require_once ROOT_PATH . 'vendor/autoload.php';
+
+$application = new \Xylo\Application\Application();
+$application->init();
+$application->executePostCallPlugins();
+$application->run();
+$application->executePostCallPlugins();
+die;
